8839d91644b334cb6cc494ca755cda2dcab6e9a6
Tunga AI <tungaventure@gmail.com>
2026-01-30 16:08:25 +0000

    m=base:328  GET https://identitytoolkit.clients6.google.com/v2/projects/

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -84,6 +84,7 @@ export default function Home() {
                     className="object-cover"
                     data-ai-hint={heroImage.imageHint}
                     priority
+                    sizes="100vw"
                 />
             )}
             <div className="absolute inset-0 bg-black/60" />
